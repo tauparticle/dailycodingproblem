f1511d476d74afd03120605070b5dc41b04baa4c
Isaiah Paradiso <isaiah.paradiso@gmail.com>
2021-08-04 18:23:46 -0700

    Init

---
+++ src/main/java/com/examples/dailycodingproblems/DailycodingproblemsApplication.java
@@ -0,0 +1,13 @@
+package com.examples.dailycodingproblems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailycodingproblemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailycodingproblemsApplication.class, args);
+	}
+
+}
